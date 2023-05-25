0ebfc0de4032e73dbf8400c7439b7def1ea3e1d3
theghostyousee <patrickserise174@gmail.com>
2023-05-25 19:13:06 +0200

    s

--- src/main.js
+++ src/main.js
@@ -1,62 +1,33 @@
 import React from 'react'
 import './main.css'
-import Arbitrum from './binance.png'
 import Twitter from './socials-twitter.svg'
 import Telegram from './socials-telegram.svg'
 import Discord from './socials-discord.svg'
 import Medium from './socials-medium.svg'
+import Logo from './logo.png'
 
 
 
 function main() {
-
-    const openTwitter = () => {
-        window.open("https://twitter.com/NPC_bsc");
-    }
-
     const tweet = () => {
-        window.open("https://twitter.com/intent/tweet?text=This%20is%20my%20proof%20of%20NPC%20for%20%40NPC_bsc%20%F0%9F%A4%96%0D%0A%0D%0ALaunching%20soon%20on%20%23BSC&")
-    }
-    const telegram = () => {
-        window.open("https://t.me/NPC_bsc")
+        window.open("https://twitter.com/intent/tweet?text=Risk%20it%20all%20for%20%40RiskProtocol%20%F0%9F%8E%B2%0D%0A& ")
     }
 
   return (
     <div className='wrap'>
 
         <div className='title'>
-            <h1>$NPC COMING<br/>on BSC.</h1>
-        </div>
-
-        <div className='paragraph'>
-            <p>The NPC meme is the most popular in the world. It deserves to be alongside PEPE<br/> even Elon Musk said so<br/></p>
+            <img src={Logo}></img>
         </div>
 
         <div className='buts'>
-            <button onClick={tweet}>Proof of NPC</button>
+            <button onClick={tweet}>REGISTER</button>
             
         </div>
 
-        <div className='little-title'>
-            <p>NETWORKS SUPPORTED</p>
-            <div className='picture'>
-                <img src={Arbitrum}></img>
-            </div>
-        </div>
-
+       
         <ul className='socials'>
-            <li onClick={openTwitter}>
-                <img src={Twitter}></img>
-            </li>
-            <li>
-                <img src={Telegram} onClick={telegram}></img>
-            </li>
-            <li>
-                <img src={Medium}></img>
-            </li>
-            {/* <li>
-                <img src={Discord}></img>
-            </li> */}
+
         </ul>
     </div>
   )
